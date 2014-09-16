204d1af402c2dc8c0edb136618e3410e7efda566
Steve Androulakis <steve.androulakis@gmail.com>
2014-09-16 11:09:58 +1000

    Testing fetching of data (need to do longer term with NodeJS for XSS issues)

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,10 +8,28 @@
  * Controller of the mytardisAngularApp
  */
 angular.module('mytardisAngularApp')
-  .controller('AboutCtrl', function ($scope) {
+  .controller('AboutCtrl', ['$scope', '$http', '$log', '$templateCache', function ($scope, $http, $log) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+	$http({method: 'GET', url: 'http://vera183.its.monash.edu.au/api/v1/experiment/?format=json'}).
+	    success(function(data, status, headers, config) {
+	      // this callback will be called asynchronously
+	      // when the response is available
+	      $scope.log = $log;
+	      
+	      $log.log('Hello World');
+	      $log.log(data);
+	    }).
+	    error(function(data, status, headers, config) {
+	      $scope.log = $log;
+
+	      $log.error('Hello Error');
+	      // called asynchronously if an error occurs
+	      // or server returns response with an error status.
+	    });  
+
+  }]);
